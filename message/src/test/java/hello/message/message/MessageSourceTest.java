0c42c2bd87a42d761f2480dd25841a64711aa816
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-04 11:22:06 +0900

    test: 메시지가 없는 경우, 기본 메시지

--- message/src/test/java/hello/message/message/MessageSourceTest.java
+++ message/src/test/java/hello/message/message/MessageSourceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -17,5 +18,16 @@ public class MessageSourceTest {
     void helloMessage() {
         String result = messageSource.getMessage("hello", null, null);
         assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode() {
+        assertThatThrownBy(() -> messageSource.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+    @Test
+    void notFoundMessageCodeDefaultMessage() {
+        String result = messageSource.getMessage("no_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지");
     }
 }
