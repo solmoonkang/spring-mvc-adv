835ec5544dee0a9ad3970d7bc57ec6386410c704
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-05 15:23:35 +0900

    feat: 로그인 id로 특정 회원 조회 로직 구현

--- login/src/main/java/hello/login/domain/member/MemberRepository.java
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -3,8 +3,7 @@ package hello.login.domain.member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Repository
@@ -22,5 +21,19 @@ public class MemberRepository {
 
     public Member findById(Long id) {
         return store.get(id);
+    }
+
+    public Optional<Member> findByLoginId(String loginId) {
+        return findAll().stream()
+                .filter(member -> member.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
