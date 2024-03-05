26135c023afb098cadc9734176c1d3b194f5c12b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-05 17:31:04 +0900

    feat: SessionConst를 사용해 로그인 컨트롤러 구현

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,10 +2,12 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -51,7 +53,6 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
     public String loginV2(@Valid @ModelAttribute LoginForm loginForm,
                           BindingResult bindingResult,
                           HttpServletResponse response) {
@@ -72,6 +73,33 @@ public class LoginController {
         sessionManager.createSession(loginMember, response);
         return "redirect:/";
     }
+
+    @PostMapping("/login")
+    public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult
+            bindingResult, HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("login? {}", loginMember);
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리
+
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+        return "redirect:/";
+    }
+
 
     public String logout(HttpServletResponse response) {
         expireCookie(response, "memberId");
