f4e94f5fc567f7dc7d6fba5f124bdbbcb011f938
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-05 15:21:16 +0900

    feat: 회원 객체 저장 로직 구현

---
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,22 @@
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        log.info("save: member={}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+}
