a8f5d85f588ac0ff8f4eba740eea063f8a55f5b6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-05 15:18:23 +0900

    feat: 로그인 프로젝트 초기 세팅 및 생성

---
+++ login/src/main/java/hello/login/web/item/ItemController.java
@@ -0,0 +1,106 @@
+package hello.login.web.item;
+
+import hello.login.domain.item.Item;
+import hello.login.domain.item.ItemRepository;
+import hello.login.web.item.form.ItemSaveForm;
+import hello.login.web.item.form.ItemUpdateForm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/login/items")
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@Validated @ModelAttribute("itemSaveForm") ItemSaveForm itemSaveForm,
+                          BindingResult bindingResult,
+                          RedirectAttributes redirectAttributes) {
+        // 특정 필드 예외가 아닌 전체 예외
+        if (itemSaveForm.getPrice() != null && itemSaveForm.getQuantity() != null) {
+            int resultPrice = itemSaveForm.getPrice() * itemSaveForm.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000,
+                        resultPrice}, null);
+            } }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "items/addForm";
+        }
+
+        //성공 로직
+        Item item = new Item(); item.setItemName(itemSaveForm.getItemName());
+        item.setPrice(itemSaveForm.getPrice());
+        item.setQuantity(itemSaveForm.getQuantity());
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/items/{itemId}";
+
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "items/editForm";
+    }
+
+    public String edit(@PathVariable Long itemId,
+                       @Validated @ModelAttribute("itemUpdateForm") ItemUpdateForm itemUpdateForm,
+                       BindingResult bindingResult) {
+        //특정 필드 예외가 아닌 전체 예외
+        if (itemUpdateForm.getPrice() != null && itemUpdateForm.getQuantity() != null) {
+            int resultPrice = itemUpdateForm.getPrice() * itemUpdateForm.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "items/editForm";
+        }
+
+        Item itemParam = new Item();
+        itemParam.setItemName(itemUpdateForm.getItemName());
+        itemParam.setPrice(itemUpdateForm.getPrice());
+        itemParam.setQuantity(itemUpdateForm.getQuantity());
+
+        itemRepository.update(itemId, itemParam);
+        return "redirect:/items/{itemId}";
+    }
+}
