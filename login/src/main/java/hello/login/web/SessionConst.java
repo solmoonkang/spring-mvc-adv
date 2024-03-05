8a04a0ef6050884bbdd9a411c3a9902b5ca13190
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-05 17:30:38 +0900

    feat: HttpSession에 데이터를 보관 및 조회할 때 사용할 상수값 정의

---
+++ login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,7 @@
+package hello.login.web;
+
+public class SessionConst {
+
+    // HttpSession에 데이터를 보관하고 조회할 때, 같은 이름이 중복 되어 사용되므로, 상수를 하나 정의한다.
+    public static final String LOGIN_MEMBER = "loginMember";
+}
