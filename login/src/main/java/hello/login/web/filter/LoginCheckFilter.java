3a77902110a77d289d68fe03435fa2e29c3422c0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-06 00:28:34 +0900

    feat: 인증 체크 필터 구현

--- login/src/main/java/hello/login/web/filter/LoginCheckFilter.java
+++ login/src/main/java/hello/login/web/filter/LoginCheckFilter.java
@@ -30,9 +30,11 @@ public class LoginCheckFilter implements Filter {
 
                 if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
                     log.info("미인증 사용자 요청 {}", requestURI);
+
+                    //로그인으로 redirect
+                    httpResponse.sendRedirect("/login?redirectURL=" + requestURI);
+                    return; //여기가 중요, 미인증 사용자는 다음으로 진행하지 않고 끝!
                 }
-                //로그인으로 redirect httpResponse.sendRedirect("/login?redirectURL=" +
-                return; //여기가 중요, 미인증 사용자는 다음으로 진행하지 않고 끝!
             }
 
             chain.doFilter(request, response);
