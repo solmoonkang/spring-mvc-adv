db61b8c1f440aeeec560d60dd6720bc081e3347a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-05 14:20:32 +0900

    chore: Item 검증 사용 코드 제거

--- validation/src/main/java/hello/validation/domain/item/Item.java
+++ validation/src/main/java/hello/validation/domain/item/Item.java
@@ -1,26 +1,16 @@
 package hello.validation.domain.item;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000)
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(9999)
     private Integer quantity;
 
     public Item() {
