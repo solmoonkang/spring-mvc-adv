2b38b2ebb8fbe21d8aa4a77cf0a247a21849c577
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-03-06 18:45:38 +0900

    test: IpPort 컨버터 테스트 코드 구현

--- typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.converter;
 
+import hello.typeconverter.type.IpPort;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,5 +19,13 @@ class ConverterTest {
         IntegerToStringConverter converter = new IntegerToStringConverter();
         String result = converter.convert(10);
         assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIpPort() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
     }
 }
